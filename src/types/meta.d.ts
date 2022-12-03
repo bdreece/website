1461b4e250ffa1dadf925a368c21c406bec04e51
Brian Reece <bdreece@mtu.edu>
2022-12-03 16:42:32 -0500

    Fixed meta tags

--- src/types/meta.d.ts
+++ src/types/meta.d.ts
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 export type MetaProps = {
-  title: string;
-  description?: string;
-  url?: URL;
+    title: string;
+    description?: string;
+    path?: string;
 };
